172a59ddb2fffe63a7dde5327f9826f2016bc1c4
Juvenal Hernandez <jjhernandezz100@gmail.com>
2022-04-12 17:45:07 -0400

    trabajando en detalle orden

--- src/api/endpoint/Endpoint.tsx
+++ src/api/endpoint/Endpoint.tsx
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.43.134:9000'
+    baseURL: 'http://192.168.1.93:5000'
 })
 
 const apis = axios.create({
--- src/screen/Ordenes.tsx
+++ src/screen/Ordenes.tsx
@@ -4,11 +4,25 @@ import api from '../api/endpoint/Endpoint'
 import { AppBar } from '../componentes/AppBar'
 import { ProductoContext } from '../context/ProductoContext'
 import { AuthContext } from '../context/AuthContext';
-import { Avatar, ListItem } from 'react-native-elements'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import { Avatar, Divider, ListItem, Overlay } from 'react-native-elements'
+import { ActivityIndicator, FlatList, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler'
 
 
 interface Props extends DrawerScreenProps<any, any> {
+}
+
+interface ordenes {
+    results: orden[]
+}
+
+interface orden {
+    id_pedido: number,
+    id_user: number,
+    monto: number,
+    fecha_creacion: string,
+    num_ref: string
+
 }
 
 
@@ -20,7 +34,7 @@ export const Ordenes = ({ navigation, route }: Props) => {
     const { user } = useContext(AuthContext)
 
 
-    const [ordenes, setOrdenes] = useState<any>([])
+    const [ordenes, setOrdenes] = useState<ordenes>()
 
     useEffect(() => {
 
@@ -31,9 +45,15 @@ export const Ordenes = ({ navigation, route }: Props) => {
     const queryOrdenes = async (id: number) => {
         const { data } = await api.post('/Ordenes', { iduser: id });
         console.log(data.results);
-        setOrdenes([data.results])
+        setOrdenes(data)
     }
 
+    const [visible, setVisible] = useState(false);
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+    const window = Dimensions.get("window");
     return (
         <>
 
@@ -45,22 +65,64 @@ export const Ordenes = ({ navigation, route }: Props) => {
                 <View style={{ flex: 1 }}>
 
                     {
-                        ordenes.map((i: any) => (
+                        ordenes.results.map((i: orden) => (
 
-                            <View key={i.id_pedido}>
-                                <ListItem key={i.id_pedido} bottomDivider hasTVPreferredFocus={undefined} tvParallaxProperties={undefined} style={{ width: '100%' }} >
+                            <TouchableOpacity key={i.id_pedido} onPress={toggleOverlay}>
+                                <ListItem bottomDivider hasTVPreferredFocus={undefined} tvParallaxProperties={undefined} style={{ width: '100%' }} >
                                     <ListItem.Content>
                                         <ListItem.Title>Pedido Numero {i.id_pedido}</ListItem.Title>
                                         <ListItem.Subtitle>Costo total: {i.monto}</ListItem.Subtitle>
                                         <ListItem.Subtitle>Referencia de pago: {i.num_ref}</ListItem.Subtitle>
                                     </ListItem.Content>
                                 </ListItem>
-                            </View>
+                            </TouchableOpacity>
 
                         ))
+                        // <Text style={{fontSize:20}}>{JSON.stringify(ordenes)}</Text>
 
 
                     }
+
+
+                    <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={{ height: window.width * 0.80 }}>
+                        <ScrollView style={{ width: window.width * 0.60 }}>
+                            <Text>Orden numero 2</Text>
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }} >
+
+                                <Text>Codigo</Text>
+                                <Text>Nombre</Text>
+                                <Text>Cantidad</Text>
+                                <Text>Precio</Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'column', justifyContent: 'space-evenly' }} >
+
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }} >
+                                    <Text>Codigo: </Text>
+                                    <Text>6090</Text>
+                                </View>
+
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }} >
+                                    <Text>Nombre: </Text>
+                                    <Text>Cambures</Text>
+                                </View>
+
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }} >
+                                    <Text>Cantidad: </Text>
+                                    <Text>3</Text>
+                                </View>
+
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }} >
+                                    <Text>Precio</Text>
+                                    <Text>$ 10</Text>
+                                </View>
+
+                            </View>
+                            <Divider orientation="vertical" width={15} />
+
+                        </ScrollView>
+                    </Overlay>
 
                 </View>
                 :
