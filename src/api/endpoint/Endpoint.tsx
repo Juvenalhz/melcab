c0238a15feb184388aad9ad22ce614dd44535a99
Juvenal Hernandez <juvenalhz95@gmail.com>
2022-05-08 18:23:10 -0400

    agg cambios pull repo remote

--- src/api/endpoint/Endpoint.tsx
+++ src/api/endpoint/Endpoint.tsx
@@ -2,11 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://tuplanetadulce.com:3000'
-=======
     baseURL: 'http://192.168.1.93:5000'
->>>>>>> 11b9a1655d15cea97cde5a4726cd094ac63c4cb2
 })
 
 const apis = axios.create({
