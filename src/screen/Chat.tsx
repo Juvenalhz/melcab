f0fb8cfdbc99917b08fb9a83b91fe57ce624bb9e
Juvenal Hernandez <juvenalhz95@gmail.com>
2022-05-08 21:32:28 -0400

    inicio de sala privada

--- src/screen/Chat.tsx
+++ src/screen/Chat.tsx
@@ -1,10 +1,14 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { Platform, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import Geolocation from '@react-native-community/geolocation';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { palette } from '../theme';
+import { Input } from 'react-native-elements/dist/input/Input';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { io } from "socket.io-client";
+const socket = io("http://192.168.43.29:9000");
 
 const styles = StyleSheet.create({
   container: {
@@ -21,97 +25,42 @@ const styles = StyleSheet.create({
 
 export function Chat() {
 
+  const sala = "PedidoCucholin";
+  const [mensajes, setmensajes] = useState<any>([]);
+  const [mensajeActual, setmensajeActual] = useState({
+    id: 1,
+    text: '',
+    nombre: 'Juvenal',
+    sala
+  })
+
   useEffect(() => {
-    Geolocation.getCurrentPosition(info => console.log(info));
+    socket.emit('entrarChat', sala)
   }, [])
+
+  useEffect(() => {
+    socket.on('mensajeRoom', (mensaje) => {
+      console.log('entra')
+      setmensajes([...mensajes, mensaje])
+    })
+
+    return() => {socket.off()}
+
+  }, [mensajes])
+
+
 
   return (
     <View style={styles.container}>
-      <GooglePlacesAutocomplete
-                placeholder="Buscar en google places"
-                minLength={6}
-                query={{
-                    key: 'AIzaSyAWBtoBHECgqiHDieEXeHmf4YbcEZLN8n0',
-                    language: 'es'
-                }}
-                textInputProps={{
-                    autoCapitalize: 'none',
-                    autoCorrect: false
-                }}
-                fetchDetails
-                enablePoweredByContainer={false}
-                styles={{
-                    container: {
-                        position: 'absolute',
-                        top: Platform.select({ ios: 60, android: 40 }),
-                        width: '100%'
-                    },
-                    textInputContainer: {
-                        marginHorizontal: 10,
-                        flex: 1,
-                        backgroundColor: 'transparent',
-                        height: 54,
-                        borderTopWidth: 0,
-                        borderBottomWidth: 0
-
-                    },
-                    textInput: {
-                        height: 54,
-                        margin: 0,
-                        padding: 0,
-                        borderRadius: 9,
-                        elevation: 5, // Shadow android
-                        shadowColor: palette.dark.main, // Shadow ios
-                        shadowOpacity: 0.1, // Shadow ios
-                        shadowOffset: { x: 0, y: 0 }, // Shadow ios
-                        shadowRadius: 15,  // Shadow ios
-                        borderWidth: 1,
-                        borderColor: palette.grayScale.gray100,
-                        fontSize: 18
-                    },
-                    listView: {
-                        marginHorizontal: 20,
-                        borderWidth: 1,
-                        borderColor: palette.grayScale.gray100,
-                        backgroundColor: palette.primary.contrastText,
-                        elevation: 5,
-                        shadowColor: palette.dark.main, // Shadow ios
-                        shadowOpacity: 0.1, // Shadow ios
-                        shadowOffset: { x: 0, y: 0 }, // Shadow ios
-                        shadowRadius: 15,  // Shadow ios
-                        marginTop: 15
-                    },
-                    description: {
-                        fontSize: 15
-                    },
-                    row: {
-                        padding: 18,
-                        height: 58
-                    }
-                }}
-
-            />
-      {/* <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-        showsUserLocation={true}
-      >
-        <Marker
-          // key={index}
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324
-          }}
-          title={'TRABAJO'}
-          description={'OFICINA'}
-        />
-      </MapView> */}
+      <View>
+        <Text>{JSON.stringify(mensajes)}</Text>
+      </View>
+      <Input value={mensajeActual.text} onChangeText={(e) => { setmensajeActual({ ...mensajeActual, text: e }) }} />
+      <TouchableOpacity onPress={() => {
+        setmensajes([...mensajes, mensajeActual])
+        socket.emit('mensajePrivado', mensajeActual);
+      }} >
+        <Text>Enviar</Text></TouchableOpacity>
     </View>
   )
 
